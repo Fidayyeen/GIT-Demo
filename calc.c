eff68d5f418f3833396d96b3a52491a1d0777641
Sakib Wani <sakibwani@gmail.com>
2025-07-15 12:14:05 +0530

    Revert "multiply added also"
    This reverts commit c8152d1b1cd3ecd28df80b7978542e461971799a.
    
    Revert "multiply added also"
    
    This reverts commit c8152d1b1cd3ecd28df80b7978542e461971799a.

--- calc.c
+++ calc.c
@@ -5,12 +5,10 @@ int sum(int a, int b)
 int diff(int a, int b)
 {
     return a-b;
-}
-int mul(int a, int b)
-{
-    return a*b;
-}
+<<<<<<< HEAD
 int div(int a, int b)
 {
     return a/b;
+=======
+>>>>>>> parent of c8152d1 (multiply added also)
 }
